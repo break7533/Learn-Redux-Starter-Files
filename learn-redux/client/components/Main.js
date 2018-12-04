9a1461f3616d7050c15d5e658a79325a393b895c
Luís Lopes <luis.lopes@armis.pt>
2018-12-04 20:02:28 +0000

    Components (Broken Main)

--- learn-redux/client/components/Main.js
+++ learn-redux/client/components/Main.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router';
 
-const Main = React.Component({
+export default class Main extends React.Component {
 	render() {
 		return (
 			<div>
 				<h1>
 					<Link>Reduxtagram</Link>
+					{React.cloneElement(this.props.children, this.props)}
 				</h1>
 			</div>
 		);
 	}
-});
-
-export default Main;
+}
