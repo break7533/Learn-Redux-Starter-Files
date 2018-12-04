2046d91d63a9f66f419af66edb69b15f46529dea
Luís Lopes <luis.lopes@armis.pt>
2018-12-04 21:55:18 +0000

    Lesson 5 - Create Redux Store

---
+++ learn-redux/client/store.js
@@ -0,0 +1,17 @@
+import { CreateStore, compse, createStore } from 'redux';
+import { syncHistoryWithStore } from 'react-router-redux';
+import { browserHistory } from 'react-router';
+import rootReducer from './reducer/index';
+import comments from './data/comments';
+import posts from './data/posts';
+
+const defaultState = {
+	posts,
+	comments
+};
+
+const store = createStore(rootReducer, defaultState);
+
+export const history = syncHistoryWithStore(browserHistory, store);
+
+export default store;
