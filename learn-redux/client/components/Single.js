3975020433d866ff8783ffa835759b723d97748e
Luís Lopes <luis.lopes@armis.pt>
2018-12-04 23:35:52 +0000

    Lesson 13 - Single Photo and Comments

--- learn-redux/client/components/Single.js
+++ learn-redux/client/components/Single.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import Photo from './Photo';
+import Comments from './Comments'
 
 export default class Single extends React.Component {
 	render() {
-		return <div className="single-photo">I'm Single</div>;
+		const i = this.props.posts.findIndex((post) => post.code === this.props.params.postId);
+		const post = this.props.posts[i];
+
+		return (
+			<div className="single-photo">
+				<Photo i={i} post={post} {...this.props} />
+				<Comments/>
+			</div>
+		);
 	}
 }
