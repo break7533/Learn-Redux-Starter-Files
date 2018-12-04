e1437d44ab9117785ed45d2f31123b51309f905d
Luís Lopes <luis.lopes@armis.pt>
2018-12-04 23:49:46 +0000

    Lesson 14 - Comments

--- learn-redux/client/components/Comments.js
+++ learn-redux/client/components/Comments.js
@@ -1,7 +1,26 @@
 import React from 'react';
 
 export default class Single extends React.Component {
+	renderComment(comment, i) {
+        return (
+            <div className="comment" key={i}>
+                <p>
+                    <strong>{comment.user}</strong>
+                    {comment.text}
+                    <button className="remove-comment">&times;</button>
+                </p>
+            </div>
+        );
+	}
+
 	render() {
-		return <div className="comment">I'mthe comments</div>;
+        return <div className="comments">
+        {this.props.postComments.map(this.renderComment)}
+        <form ref="commentForm" className="comment-form">
+            <input type="text" ref="author" placeholder="Author"/>
+            <input type="text" ref="comment" placeholder="Comment"/>
+            <input type="submit" hidden/>
+        </form>
+        </div>;
 	}
 }
