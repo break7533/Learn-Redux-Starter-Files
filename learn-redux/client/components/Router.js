0917bc99e6a33d460374911060ec3b2ea11949be
Luís Lopes <luis.lopes@armis.pt>
2018-12-04 22:34:20 +0000

    Lesson 10 - Connect State and Dispach to Main

--- learn-redux/client/components/Router.js
+++ learn-redux/client/components/Router.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Router, Route, IndexRoute } from 'react-router';
-import Main from './Main';
+import App from './App';
 import Single from './Single';
 import PhotoGrid from './PhotoGrid';
 import { Provider } from 'react-redux';
@@ -11,7 +11,7 @@ export default class BRouter extends React.Component {
 		return (
 			<Provider store={store}>
 			<Router history={history}>
-				<Route path="/" component={Main}>
+				<Route path="/" component={App}>
 					<IndexRoute component={PhotoGrid} />
 					<Route path="/view/:postId" component={Single} />
 				</Route>
