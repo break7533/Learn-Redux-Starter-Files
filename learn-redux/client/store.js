1b0d67d148ac0dc4d23c78f5a6f8c330ef86c2fd
Luís Lopes <luis.lopes@armis.pt>
2018-12-04 22:13:21 +0000

    Lesson 7 - Redux Reducers

--- learn-redux/client/store.js
+++ learn-redux/client/store.js
@@ -1,7 +1,7 @@
 import { CreateStore, compse, createStore } from 'redux';
 import { syncHistoryWithStore } from 'react-router-redux';
 import { browserHistory } from 'react-router';
-import rootReducer from './reducer/index';
+import rootReducer from './reducers/index';
 import comments from './data/comments';
 import posts from './data/posts';
 
