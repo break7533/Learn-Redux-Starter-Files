46debf385155ff769e92d9c7bf20c67bb7a75bb5
Luís Lopes <luis.lopes@armis.pt>
2018-12-05 18:18:58 +0000

    Lesson 17 - Setting up Sentry

--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -2,5 +2,21 @@ import React from 'react';
 import { render } from 'react-dom';
 import BRouter from './components/Router';
 import css from './styles/style.styl';
+import Raven from 'raven-js';
+import { sentry_url, logException } from './data/config';
+
+Raven.config(sentry_url, {
+	tags: {
+		git_commit: '1234sas',
+		userLevel: 'editor'
+	}
+}).install();
+
+logException(new Error('download failed'), {
+	email: 'wesbos@gmail.com'
+});
+
+Raven.captureMessage('Common error, just message me');
+Raven.showReportDialog();
 
 render(<BRouter />, document.getElementById('root'));
